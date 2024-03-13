024c226cccac2eda538bbb65646aa4f6f39675f4
zhephyn <zhephynwinter@gmail.com>
2024-03-13 17:50:05 +0300

    created controller actions; idex, show, new , create, update and destroy for working with the post model. Also created erb view files for index, show and new actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            ##render the show page to see the created post
+        else
+            ##render a  new form to create a new post again
+        end
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
