db45d64d86a6ff65510fc2df4ee69c89a2230ba0
zhephyn <zhephynwinter@gmail.com>
2024-03-19 04:16:09 +0300

    edited the comments controller to allow CRUD operations for the comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,38 @@
 class CommentsController < ApplicationController
     def index
-        @post = Post.find(params[:id])
-        @comments = @post.comments.find(params[:id])
+        @post = Post.find(params[:post_id])
+        @comments = @post.comments
     end 
 
     def new
-        @post = Post.find(params[:id])
+        @post = Post.find(params[:post_id])
         @comment = @post.comments.build(comment_params)
     end
 
+    def show
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+    end
     def create
-        @post = Post.find(params[:id])
+        @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         if @comment.save
             redirect_to post_path(@post)
         else
             render :new
-    end
-    def destroy
-        @post = Post.find(params[:id])
-        @comment = @post.comments.find(params[:id])
-        if @comment.destroy
-            redirect_to posts_path
         end
     end
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        if @comment.destroy
+            redirect_to post_path(@post)
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:body)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_17_031420) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_18_232245) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_031420) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_031420) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
 end
