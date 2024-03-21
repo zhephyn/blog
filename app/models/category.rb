0fcc35565cbd42f312c63e0e16abd7e95a0e082f
zhephyn <zhephynwinter@gmail.com>
2024-03-21 11:02:19 +0300

    added associations for the Post and Category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
     ##add associations with the post model
+    has_many :posts
     ##create  a migration to add category_id to the posts table 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :category
     has_many :comments
     has_one_attached :image
     validates :title, presence: true, length: {minimum: 1, maximum: 100}
